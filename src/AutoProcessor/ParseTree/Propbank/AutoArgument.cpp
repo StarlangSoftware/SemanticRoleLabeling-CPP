4d56502483fb022c0511ae8b119731e4185f9f26
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-07-12 14:55:01 +0300

    Updated the documentation.

--- src/AutoProcessor/ParseTree/Propbank/AutoArgument.cpp
+++ src/AutoProcessor/ParseTree/Propbank/AutoArgument.cpp
@@ -10,6 +10,14 @@ AutoArgument::AutoArgument(ViewLayerType secondLanguage) {
     this->secondLanguage = secondLanguage;
 }
 
+/**
+ * Given the parse tree and the frame net, the method collects all leaf nodes and tries to set a propbank argument
+ * label to them. Specifically it tries all possible argument types one by one ARG0 first, then ARG1, then ARG2 etc.
+ * Each argument type has a special function to accept. The special function checks basically if there is a specific
+ * type of ancestor (specific to the argument, for example SUBJ for ARG0), or not.
+ * @param parseTree Parse tree for semantic role labeling
+ * @param frameset Frame net used in labeling.
+ */
 void AutoArgument::autoArgument(ParseTreeDrawable* parseTree, Frameset frameset) {
     ArgumentType values[] = {ArgumentType::NONE, ArgumentType::PREDICATE, ArgumentType::ARG0, ArgumentType::ARG1, ArgumentType::ARG2, ArgumentType::ARG3, ArgumentType::ARG4, ArgumentType::ARG5, ArgumentType::ARGMNONE, ArgumentType::ARGMEXT, ArgumentType::ARGMLOC, ArgumentType::ARGMDIS, ArgumentType::ARGMADV, ArgumentType::ARGMCAU, ArgumentType::ARGMTMP, ArgumentType::ARGMPNC, ArgumentType::ARGMMNR, ArgumentType::ARGMDIR};
     NodeDrawableCollector nodeDrawableCollector = NodeDrawableCollector((ParseNodeDrawable*) parseTree->getRoot(), new IsTransferable(secondLanguage));
--- src/AutoProcessor/ParseTree/Propbank/TurkishAutoArgument.cpp
+++ src/AutoProcessor/ParseTree/Propbank/TurkishAutoArgument.cpp
@@ -4,10 +4,19 @@
 
 #include "TurkishAutoArgument.h"
 
+/**
+ * Sets the language.
+ */
 TurkishAutoArgument::TurkishAutoArgument() : AutoArgument(ViewLayerType::TURKISH_WORD){
-
 }
 
+/**
+ * Checks all ancestors of the current parse node, until an ancestor has a tag of given name, or the ancestor is
+ * null. Returns the ancestor with the given tag, or null.
+ * @param parseNode Parse node to start checking ancestors.
+ * @param name Tag to check.
+ * @return The ancestor of the given parse node with the given tag, if such ancestor does not exist, returns null.
+ */
 bool TurkishAutoArgument::checkAncestors(ParseNode* parseNode, const string &name) {
     while (parseNode != nullptr){
         if (parseNode->getData().getName() == name){
@@ -18,6 +27,14 @@ bool TurkishAutoArgument::checkAncestors(ParseNode* parseNode, const string &nam
     return false;
 }
 
+/**
+ * Checks all ancestors of the current parse node, until an ancestor has a tag with the given, or the ancestor is
+ * null. Returns the ancestor with the tag having the given suffix, or null.
+ * @param parseNode Parse node to start checking ancestors.
+ * @param suffix Suffix of the tag to check.
+ * @return The ancestor of the given parse node with the tag having the given suffix, if such ancestor does not
+ * exist, returns null.
+ */
 bool TurkishAutoArgument::checkAncestorsUntil(ParseNode* parseNode, const string &suffix) {
     while (parseNode != nullptr){
         if (parseNode->getData().getName().find("-" + suffix) != string::npos){
@@ -28,6 +45,13 @@ bool TurkishAutoArgument::checkAncestorsUntil(ParseNode* parseNode, const string
     return false;
 }
 
+/**
+ * The method tries to set the argument of the given parse node to the given argument type automatically. If the
+ * argument type condition matches the parse node, it returns true, otherwise it returns false.
+ * @param parseNode Parse node to check for semantic role.
+ * @param argumentType Semantic role to check.
+ * @return True, if the argument type condition matches the parse node, false otherwise.
+ */
 bool TurkishAutoArgument::autoDetectArgument(ParseNodeDrawable *parseNode, ArgumentType argumentType) {
     ParseNode* parent = parseNode->getParent();
     switch (argumentType){
